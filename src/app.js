70cb5676b075b469da330c1842165e60cefe23c3
ThiagoSuchi <thiagohenssuchi@gmail.com>
2025-10-06 15:20:26 -0400

    Ajustes em server.js e app.js, correção de sintaxe para o aumento de performance da api

--- src/app.js
+++ src/app.js
@@ -1,28 +1,23 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import scoreRoutes from "./routes/scoreRoutes.js"; 
+
+dotenv.config();
 
 const app = express();
 
-
-app.use(cors({
-  origin: "http://localhost:3000", 
-}));
+app.use(cors());
 app.use(express.json()); 
 
-
-mongoose.connect(
-"mongodb+srv://joaosports19_db_user:k8qnAoyD2xDOBLLj@cluster0.xp29en9.mongodb.net/"
-)
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("MongoDB conectado"))
 .catch(err => console.error("Erro ao conectar no MongoDB:", err));
 
-
 app.use("/", scoreRoutes);
 
-
-app.get("/teste", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "API funcionando!" });
 });
 
