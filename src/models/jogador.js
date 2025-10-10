1543bbde653835e67dc8be7a65be085546f0f92c
ThiagoSuchi <thiagohenssuchi@gmail.com>
2025-10-09 22:31:08 -0400

    Rota get implementada para fluxo de login e cadastro

--- src/models/jogador.js
+++ src/models/jogador.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const jogadorSchema = new mongoose.Schema({
-  name: { type: String, required: true},
+  name: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   score: { type: Number, default: 0 },
 });
