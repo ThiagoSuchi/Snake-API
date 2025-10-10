6bc6941e54b1600a17192cf2098f0838844f5b59
joaovkm06 <joaosports19@gmail.com>
2025-10-10 14:26:13 -0400

    adicionando o swager

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import scoreRoutes from "./routes/scoreRoutes.js"; 
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path, { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const swaggerDocument = YAML.load(join(__dirname, "..", "swagger.yaml"));
 
 dotenv.config();
 
@@ -16,6 +24,7 @@ mongoose.connect(process.env.MONGODB_URI)
 .catch(err => console.error("Erro ao conectar no MongoDB:", err));
 
 app.use("/", scoreRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.json({ message: "API funcionando!" });
